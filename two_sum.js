d4c72da24bc14b4db2b726a1c0064bcabba481a3
LazyYeou <lazyyeou@gmail.com>
2025-05-17 20:20:37 +0700

    Create two_sum.js

---
+++ two_sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++){
+        let find = target - nums[i]
+        if (map.has(find)){
+            return [map.get(find), i]
+        }else{
+            map.set(nums[i], i)
+        }
+    }
+    
+    
+};
